f723de9b6b8c62dac8410d9ed8fcfd3c266bfd41
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-03-02 15:20:16 -0500

    Added comments regarding code

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-// ## User Story
-
-// ```md
-// AS A developer
-// I WANT a README generator
-// SO THAT I can quickly create a professional README for a new project
-// ```
-
-// ## Acceptance Criteria
-
-// ```md
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I enter my project title
-// THEN this is displayed as the title of the README
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
-// ```
-
-
 // TODO: Include packages needed for this application
 
 const inquirer = require("inquirer");
@@ -115,10 +86,16 @@ const questions = [
         },
     ];
 
+// created prompt questions
+// ==============================================================================================================================================================
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, generateMarkdown(data), (err) => err ? console.error(err) : console.log("Success!"))
 }
+
+// created function writeToFile where fileName and data are parameters, if there is an arror, console will log the error, else console will log success once completed
+// ==============================================================================================================================================================
 
 // TODO: Create a function to initialize app
 function init() {
@@ -130,6 +107,9 @@ function init() {
             console.error(error);
         });
 }
+
+// created function init, the inquirer prompt questions will have data, writeToFile creates README.md with the data that comes from the prompt and will catch any errors along the way
+// ==============================================================================================================================================================
 
 // Function call to initialize app
 init();
